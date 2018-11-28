a64e43231aabf0e3cb5d5a77410fe5988749d527
omar-moreno <email@omarmoreno.net>
2018-11-28 14:34:16 -0800

    Class for managing IO files.

---
+++ processing/include/EventFile.h
@@ -0,0 +1,75 @@
+/**
+ * @file EventFile.h
+ * @brief Class for managing io files.
+ * @author Omar Moreno, SLAC National Accelerator Laboratory
+ */
+
+#ifndef __EVENT_FILE_H__
+#define __EVENT_FILE_H__
+
+//----------//
+//   ROOT   //
+//----------//
+#include <TFile.h>
+#include <TTree.h>
+
+//----------//
+//   LCIO   //
+//----------//
+#include <IO/LCReader.h>
+#include <IOIMPL/LCFactory.h>
+#include <EVENT/LCEvent.h>
+
+//-----------//
+//   hpstr   //
+//-----------//
+#include "Event.h"
+
+class EventFile { 
+
+    public: 
+
+        /** Constructor */
+        EventFile(const std::string ifilename, const std::string ofilename);
+
+        /** Destructor */
+        ~EventFile();
+
+        /**
+         * Load the next event in the file. 
+         *
+         * @return true if an event was loaded successfully, false otherwise 
+         */
+        bool nextEvent(); 
+
+        /**
+         * Setup the event object that will be used by this file.
+         *
+         * @param event The Event container.
+         */
+        void setupEvent(Event* event); 
+
+        /** 
+         * Close the file, writing the tree to disk if creating an output file.
+         */
+        void close();
+
+    private: 
+
+        /** The ROOT file to which event data will be written to. */
+        TFile* ofile_{nullptr}; 
+
+        /** Object used to build the HPS event model. */
+        Event* event_{nullptr};
+
+        /** Object used to load all of current LCIO event information. */
+        EVENT::LCEvent* lc_event_{nullptr}; 
+        
+        /** LCIO reader */
+        IO::LCReader* lc_reader_{IOIMPL::LCFactory::getInstance()->createLCReader()}; 
+
+        int entry_{0};  
+
+}; // EventFile
+
+#endif // __EVENT_FILE_H__
