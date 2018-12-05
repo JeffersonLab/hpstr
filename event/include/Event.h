27900537aa5c023d3e21669135becbaef17e56c8
omar-moreno <email@omarmoreno.net>
2018-12-04 22:34:50 -0800

    Add method to set LCEvent.  Only add a collection to the event if it hasn't been added yet.

--- event/include/Event.h
+++ event/include/Event.h
@@ -9,6 +9,12 @@
 #define __EVENT_H__
 
 //----------//
+//   LCIO   //
+//----------//
+#include <EVENT/LCCollection.h>
+#include <EVENT/LCEvent.h>
+
+//----------//
 //   ROOT   //
 //----------//
 #include <TClonesArray.h>
@@ -32,13 +38,33 @@ class Event {
          */
         void addCollection(const std::string name, TClonesArray* collection);
 
+    
+        /**
+         * Clear all of the collections in the event 
+         */
+        void Clear(); 
+
         /** @return The ROOT tree containing the event. */
         TTree* getTree() { return tree_; }
+
+        /** Set the LCIO event. */
+        void setLCEvent(EVENT::LCEvent* lc_event) { lc_event_ = lc_event; }; 
+
+        /** Get the LCIO event. */
+        EVENT::LCCollection* getLCCollection(std::string name) { 
+            return static_cast<EVENT::LCCollection*>(lc_event_->getCollection(name)); 
+        }; 
 
     private: 
 
         /** The ROOT tree containing the event. */
         TTree* tree_{nullptr}; 
+        
+        /** Object used to load all of current LCIO event information. */
+        EVENT::LCEvent* lc_event_{nullptr};
+
+        /** Container with all TClonesArray collections. */
+        std::map<std::string, TClonesArray*> collections_;  
 
 }; // Event
 
