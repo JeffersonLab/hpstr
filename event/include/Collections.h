289c5fb56459555ba79a1d09e791d0faf07897d2
omar-moreno <email@omarmoreno.net>
2018-12-06 17:31:32 -0800

    Header with collection names.

---
+++ event/include/Collections.h
@@ -0,0 +1,32 @@
+/**
+ * @file Collection.h
+ * @brief List of collection names.
+ * @author Omar Moreno, SLAC National Accelerator Laboratory
+ */
+
+#ifndef _COLLECTIONS_H_
+#define _COLLECTIONS_H_
+
+namespace Collections { 
+
+    /** Name of the tracks collection. */
+    constexpr const char* HPS_TRACKS{"GBLTracks"};
+
+    /** Name of the tracker hits collection. */
+    constexpr const char* HPS_TRACKER_HITS{"RotatedHelicalTrackHits"};
+
+    /** The name of the collection containing GBL kink data GenericObjects */
+    constexpr const char* KINK_DATA{"GBLKinkData"};
+
+    /** Name of the collection containing Track to GBLKinkData LCRelations. */
+    constexpr const char* KINK_DATA_REL{"GBLKinkDataRelations"};
+
+    /** Name of the collection of TrackData GenericObjects */ 
+    constexpr const char* TRACK_DATA{"TrackData"}; 
+
+    /** Name of the collection containing Track to TrackData LCRelations */
+    constexpr const char* TRACK_DATA_REL{"TrackDataRelations"};
+
+}
+
+#endif // _COLLECTION_H_
