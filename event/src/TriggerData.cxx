117a6e2f029306af0be8734375725381be38a0fb
omar-moreno <email@omarmoreno.net>
2018-12-14 08:53:55 -0800

    Forgot to add underscores to variable names.

--- event/src/TriggerData.cxx
+++ event/src/TriggerData.cxx
@@ -13,11 +13,11 @@ TriggerData::TriggerData(EVENT::LCGenericObject* trigger_data) {
 void TriggerData::parseTriggerData(EVENT::LCGenericObject* trigger_data) { 
 
     int trigger_data_int = trigger_data->getIntVal(1);
-    single0 = ((trigger_data_int >> 24) & 1) == 1;
-    single1 = ((trigger_data_int >> 25) & 1) == 1;
-    pair0 = ((trigger_data_int >> 26) & 1) == 1;
-    pair1 = ((trigger_data_int >> 27) & 1) == 1;
-    pulser = ((trigger_data_int >> 29) & 1) == 1;
+    single0_ = ((trigger_data_int >> 24) & 1) == 1;
+    single1_ = ((trigger_data_int >> 25) & 1) == 1;
+    pair0_ = ((trigger_data_int >> 26) & 1) == 1;
+    pair1_ = ((trigger_data_int >> 27) & 1) == 1;
+    pulser_ = ((trigger_data_int >> 29) & 1) == 1;
 
     trigger_data_int = trigger_data->getIntVal(3);
     long w1 = trigger_data_int & 0xffffffffL;
@@ -27,5 +27,5 @@ void TriggerData::parseTriggerData(EVENT::LCGenericObject* trigger_data) {
     long timelo = w1;
     long timehi = (w2 & 0xffff) << 32;
 
-    time_stamp = 4 * (timelo + timehi); 
+    time_stamp_ = 4 * (timelo + timehi); 
 }
