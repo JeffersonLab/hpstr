9d9764376d5735c80e44226cde20b4e421454d63
omar-moreno <email@omarmoreno.net>
2018-12-14 08:02:16 -0800

    Add EventHeader to ROOT dictionary.

--- event/include/EventDef.h
+++ event/include/EventDef.h
@@ -2,6 +2,7 @@
 #include "CalCluster.h"
 #include "CalHit.h"
 #include "Event.h"
+#include "EventHeader.h"
 #include "Particle.h"
 #include "Track.h"
 #include "TrackerHit.h"
--- event/include/EventLinkDef.h
+++ event/include/EventLinkDef.h
@@ -8,6 +8,7 @@
 
 #pragma link C++ class CalCluster+;
 #pragma link C++ class CalHit+;
+#pragma link C++ class EventHeader+; 
 #pragma link C++ class Particle+; 
 #pragma link C++ class Track+;
 #pragma link C++ class TrackerHit+;
