fd2e7ac83c2458ec2213ee04c3b6d618061202f6
Alic Shen Spellman <alspellm@cent7c.slac.stanford.edu>
2019-10-23 10:15:45 -0700

    add setup.sh  back

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+#export PYTHONPATH=/data/src/hpstr/processing/python:$PYTHONPATH
+export PYTHONPATH=/data/src/hpstr/pyplot:/data/src/hpstr/install/lib/python:$PYTHONPATH
+
+export PATH=/data/src/hpstr/install/bin:$PATH
+export LD_LIBRARY_PATH=/data/src/hpstr/install/lib:$LD_LIBRARY_PATH
