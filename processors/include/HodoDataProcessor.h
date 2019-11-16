29ceeaa882823bf37c2762bf81ec43f2419e8191
Maurik Holtrop <maurik@physics.unh.edu>
2019-11-15 19:34:20 -0500

    Temp commit

--- processors/include/HodoDataProcessor.h
+++ processors/include/HodoDataProcessor.h
@@ -2,7 +2,7 @@
  *  @file   HodoDataProcessor.cxx
  *  @brief  Processor for the Hodoscope, to convert LCIO to the HodoCluster and HodoHit classes in event.
  *  @author Maurik Holtrop, University of New Hampshire, 11/13/19.
-*/
+ */
 
 #ifndef HodoDataProcessor_h
 #define HodoDataProcessor_h
@@ -28,61 +28,63 @@ class TTree;
 
 constexpr const char* HODO_HITS{"HodoCalHits"};
 constexpr const char* HODO_GENERIC_HITS{"HodoGenericHits"};
+constexpr const char* HODO_CLUSTERS{"HodoCluster"};
 
 class HodoDataProcessor : public Processor {
-
-    public:
-
-        /**
-         * Class constructor.
-         *
-         * @param name Name for this instance of the class.
-         * @param process The Process class associated with Processor, provided
-         *                by the processing framework.
-         */
-        HodoDataProcessor(const std::string& name, Process& process);
-
-        /** Destructor */
-        ~HodoDataProcessor();
-
-        /**
-         * Process the event and put new data products into it.
-         * @param event The Event to process.
-         */
-        virtual bool process(IEvent* event);
-
-        /**
-         * Callback for the Processor to take any necessary
-         * action when the processing of events starts.
-         */
-        virtual void initialize(TTree* tree);
-
-        /**
-         * Callback for the Processor to take any necessary
-         * action when the processing of events finishes.
-         */
-        virtual void finalize();
-
-    private:
-
-        /**
-         * Method to unpack field value from a calorimeter hit ID.
-         *
-         * @param field The field ID to unpack
-         * @param hit The CalorimeterHit whose ID will be used to unpack the
-         *            the field value.
-         */
-        UTIL::BitFieldValue getIdentifierFieldValue(std::string field, EVENT::CalorimeterHit* hit);
-
-        /** TClonesArray collection containing all ECal hits. */
-        std::vector<HodoHit*> cal_hits_;
-
-        /** TClonesArray collection containing all ECal clusters. */
-        std::vector<HodoCluster*> clusters_;
-
-        /** Encoding string describing cell ID. */
-        const std::string encoder_string_{"system:6,barrel:3,layer:4,ix:4,iy:-3,hole:-3"};
-
+    
+public:
+    
+    /**
+     * Class constructor.
+     *
+     * @param name Name for this instance of the class.
+     * @param process The Process class associated with Processor, provided
+     *                by the processing framework.
+     */
+    HodoDataProcessor(const std::string& name, Process& process): Processor(name, process) {};
+    
+    /** Destructor */
+    ~HodoDataProcessor(){};
+    
+    /**
+     * Process the event and put new data products into it.
+     * @param event The Event to process.
+     */
+    virtual bool process(IEvent* event);
+    
+    /**
+     * Callback for the Processor to take any necessary
+     * action when the processing of events starts.
+     */
+    virtual void initialize(TTree* tree);
+    
+    /**
+     * Callback for the Processor to take any necessary
+     * action when the processing of events finishes.
+     */
+    virtual void finalize(){};
+    
+private:
+    
+    /**
+     * Method to unpack field value from a hodoscope hit ID from the ID string.
+     * The string itself comes from the detector description (in hps-java/detector-data/...), and should be identical to that one.
+     *
+     * @param field The field ID to unpack
+     * @param hit The CalorimeterHit whose ID will be used to unpack the
+     *            the field value.
+     */
+    UTIL::BitFieldValue getIdentifierFieldValue(std::string field, EVENT::CalorimeterHit* hit);
+    
+    /** TClonesArray collection containing all ECal hits. */
+    std::vector<HodoHit*> cal_hits_;
+    
+    /** TClonesArray collection containing all ECal clusters. */
+    std::vector<HodoCluster*> clusters_;
+    
+    /** Encoding string describing cell ID. */
+    const std::string encoder_string_{"system:6,barrel:3,layer:4,ix:4,iy:-3,hole:-3"};
+    
 }; // ECalDataProcessor
 
 
--- processors/src/HodoDataProcessor.cxx
+++ processors/src/HodoDataProcessor.cxx
@@ -6,3 +6,39 @@
 
 #include "HodoDataProcessor.h"
 
+void HodoDataProcessor::initialize(TTree* tree) {
+    tree->Branch(HODO_HITS, &cal_hits_);
+    tree->Branch(HODO_CLUSTERS, &clusters_);
+}
+
+bool HodoDataProcessor::process(IEvent* ievent) {
+  
+  // Clean up.
+  for(int i = 0; i < cal_hits_.size(); i++) delete cal_hits_.at(i);
+  for(int i = 0; i < clusters_.size(); i++) delete clusters_.at(i);
+  cal_hits_.clear();
+  clusters_.clear();
+
+  // Interface to implementation casst. Not sure why we bother with an interface.
+  Event* event = static_cast<Event*> (ievent);
+  
+  EVENT::LCCollection* hits{nullptr};
+  try{
+    hits =  static_cast<EVENT::LCCollection*>(event->getLCCollection(HODO_HITS));
+  }catch(EVENT::DataNotAvailableException e){
+    std::cout << "Barfed on not finding the hodoscope collections. \n";
+  }
+  
+  std::cout << "Hodo n-hits: " << hits->getNumberOfElements() << std::endl;
+  
+  return true;
+}
+
+UTIL::BitFieldValue HodoDataProcessor::getIdentifierFieldValue(std::string field, EVENT::CalorimeterHit* hit){
+
+    UTIL::BitField64 decoder(encoder_string_);
+    long long value = (long long)( hit->getCellID0() & 0xffffffff ) | ( (long long)( hit->getCellID1() ) << 32 ) ;
+    decoder.setValue(value);
+
+    return decoder[field];
+}
