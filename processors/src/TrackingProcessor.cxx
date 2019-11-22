0a55b9b0433b837a73f5464b94d0f83eb6c048eb
Pierfrancesco Butti <pbutti@cent7c.slac.stanford.edu>
2019-11-21 17:29:09 -0800

    Remove silly clear

--- processors/src/TrackingProcessor.cxx
+++ processors/src/TrackingProcessor.cxx
@@ -42,10 +42,6 @@ bool TrackingProcessor::process(IEvent* ievent) {
     }
 
 
-    tracks_.clear();
-    hits_.clear();
-    rawhits_.clear();
-
     Event* event = static_cast<Event*> (ievent);
     // Get the collection of 3D hits from the LCIO event. If no such collection 
     // exist, a DataNotAvailableException is thrown
