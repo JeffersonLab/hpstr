d7f0cdd3c2eb7b2c244d9643df8f7fbd3114465f
Cameron Bravo <bravo@slac.stanford.edu>
2019-11-23 18:29:06 -0800

    Add anaRecoTuple_cfg.py

---
+++ processors/config/anaRecoTuple_cfg.py
@@ -0,0 +1,42 @@
+import HpstrConf
+import sys
+
+# Use the input file to set the output file name
+infile = sys.argv[1].strip()
+outfile = '%s_anaReco.root' % infile[:-5]
+
+print 'Input file: %s' % infile
+print 'Output file: %s' % outfile
+
+p = HpstrConf.Process()
+
+# Library containing processors
+p.libraries.append("libprocessors.so")
+
+###############################
+#          Processors         #
+###############################
+
+recoana = HpstrConf.Processor('recoana', 'RecoHitAnaProcessor')
+
+###############################
+#   Processor Configuration   #
+###############################
+#RecoHitAna
+recoana.parameters["debug"] = 0
+recoana.parameters["anaName"] = "recoHitAna"
+recoana.parameters["trkColl"] = "GBLTracks"
+recoana.parameters["trkrHitColl"] = "RotatedHelicalTrackHits"
+recoana.parameters["ecalHitColl"] = "RecoEcalHits"
+recoana.parameters["ecalClusColl"] = "RecoEcalClusters"
+
+
+# Sequence which the processors will run.
+p.sequence = [recoana]
+
+p.input_files=[infile]
+p.output_files = [outfile]
+
+#p.max_events = 1000
+
+p.printProcess()
