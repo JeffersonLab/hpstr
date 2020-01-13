15bb698115e35c822869a919afa6f711920770d9
alspellm <alspellm@ucsc.edu>
2020-01-13 13:17:19 -0800

    added counting per event in Svt2DBlHistos.cxx FillHistograms

--- analysis/src/Svt2DBlHistos.cxx
+++ analysis/src/Svt2DBlHistos.cxx
@@ -26,7 +26,38 @@ void Svt2DBlHistos::FillHistograms(std::vector<RawSvtHit*> *rawSvtHits_,float we
     std::string histokey;
     if(Event_number%1000 == 0) std::cout << "Event: " << Event_number 
         << " Number of RawSvtHits: " << nhits << std::endl;
+
+    //Following Block counts the total number of hits each hybrid records per event
+    int matrix[4][15]= {0};
     for (int i = 0; i < nhits; i++)
+    {
+        RawSvtHit* rawSvtHit = rawSvtHits_->at(i);
+        int mod = rawSvtHit->getModule();
+        int lay = rawSvtHit->getLayer();
+        //std::cout << "module: " << mod << std::endl;
+        matrix[mod][lay]++;
+
+    }
+    for (int i =0; i < 4; i++)
+    {
+        for (int j = 1; j < 15; j++)
+        {
+            if (j<9 && i>1)
+            {
+            }
+            else
+            {   
+                std::string swTag= mmapper_->getStringFromSw("ly"+std::to_string(j)+"_m"+std::to_string(i));
+                Fill1DHisto("Count_"+swTag+"_h", matrix[i][j],weight);
+            }
+        }
+    }
+
+    Fill1DHisto("DetectorTotalHits_h", nhits,weight);
+    //End of counting block
+
+    //Populates histograms for each hybrid
+    /*for (int i = 0; i < nhits; i++)
     {
         //std::cout << "hit: " << i << std::endl;
         RawSvtHit* rawSvtHit = rawSvtHits_->at(i);
@@ -36,29 +67,19 @@ void Svt2DBlHistos::FillHistograms(std::vector<RawSvtHit*> *rawSvtHits_,float we
         std::string swTag= mmapper_->getStringFromSw("ly"+lay+"_m"+mod);
         //std::cout << swTag << std::endl;
         //std::cout << histokey << std::endl;
+        
+        
         for(int ss = 0; ss < 6; ss++)
         {
             histokey = "baseline"+std::to_string(ss)+"_"+swTag+"_hh";
             Fill2DHisto(histokey, 
                     (float)rawSvtHit->getStrip(),
-                    (float)rawSvtHit->getADCs()[0], 
+                    (float)rawSvtHit->getADCs()[ss], 
                     weight);
         }
-    }
+    }*/
     Event_number++;
 
-
-    /*
-       std::string swTag="";
-       std::string build_key = "";
-       auto mod = std::to_string(rawSvtHit->getModule());
-       auto lay = std::to_string(rawSvtHit->getLayer());
-       swTag= mmapper_->getStringFromSw("ly"+lay+"_m"+mod);
-       for (int i=0; i<6; i++){
-       build_key= m_name+"_"+swTag+"_timesample_"+std::to_string(i);
-       histos2d[build_key]->Fill((float)rawSvtHit->getStrip(),(float)rawSvtHit->getADCs()[i], weight); 
-       }*/
-
-
+    
 
 }      
