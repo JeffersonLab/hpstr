86105d4773aca8859e2c17c32249c164bcce6f6b
Alic <alspellm@ucsc.edu>
2020-02-12 15:28:16 -0800

    cleaned up the SvtBlFitHistoProcessor by moving code into analysis/src/BlFitHistos.h. Still need to fix overflow issue

--- analysis/include/BlFitHistos.h
+++ analysis/include/BlFitHistos.h
@@ -9,9 +9,9 @@
 #include "TH1.h"
 #include "TrackerHit.h"
 #include "RawSvtHit.h"
-
+#include "HistoManager.h"
 #include "ModuleMapper.h"
-
+#include "TF1.h"
 #include <string>
 
 class BlFitHistos : public HistoManager{
@@ -20,12 +20,15 @@ class BlFitHistos : public HistoManager{
         BlFitHistos(const std::string& inputName);
         ~BlFitHistos();
     
+        void Chi2GausFit( HistoManager* inputHistos_, BlFitHistos* outputHistos_, int nPointsDer_,int rebin_);
         void FillHistograms();
         
     private:
         
         TH1F* fitHistos{nullptr};
         std::map<std::string, TGraphErrors*> baselineGraphs;
+        //int rebin_{};
+        //int nPointsDer_{};
 };
 
 #endif
