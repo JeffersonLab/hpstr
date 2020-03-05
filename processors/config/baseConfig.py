c955365651b78aa040dca9c10e58b6084acdac76
Pierfrancesco Butti <pierfrancesco.butti@gmail.com>
2020-03-05 11:03:30 -0800

    Added checks on the collection names: if empty do not attempt to retrieve them

--- processors/config/baseConfig.py
+++ processors/config/baseConfig.py
@@ -10,3 +10,7 @@ parser.add_option("-o", "--outFile", type="string", dest="outFilename",
                   help="Output filename.", metavar="outFilename", default="out.root")
 parser.add_option("-t", "--isData", type="int", dest="isData",
                   help="Type of lcio ntuple: 1=data, 0=MC", metavar="isData", default=-1)
+parser.add_option("-y", "--year", type="int", dest="year",
+                  help="Select year of the data", metavar="year", default=2019)
+parser.add_option("-n", "--nevents", type="int", dest="nevents",
+                  help="Number of events to process", metavar="nevents",default=-1)
