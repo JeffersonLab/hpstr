a5638ec0fec9111b9de9669348ad88c157bf0bf5
Stanislava Sevova <ssevova@stanford.edu>
2020-05-12 04:13:06 -0700

    minor changes to Make2DPlots, MakePlot

--- plotUtils/compareMC.py
+++ plotUtils/compareMC.py
@@ -8,21 +8,23 @@ utils.SetStyle()
 path = "/gpfs/slac/atlas/fs1/d/ssevova/hps/hpstr/histos"
 inFileList = [
     "tritrig/tritrig_anaVtx.root",
+    "wab/wab_anaVtx.root",
     "simp/simp_100_60_33p3_recon_ctau10mm_anaVtx.root",
-    "simp/simp_130_78_43p3_recon_ctau10mm_anaVtx.root",
-    "simp/simp_100_40_30_recon_ctau10mm_anaVtx.root",
-    "simp/simp_50_30_16p7_recon_ctau10mm_anaVtx.root"]
+    "simp/simp_130_78_43p3_recon_ctau10mm_anaVtx.root"]
+ #   "simp/simp_100_40_30_recon_ctau10mm_anaVtx.root",
+ #   "simp/simp_50_30_16p7_recon_ctau10mm_anaVtx.root"]
 
 
 colors = [r.kBlack, r.kRed, r.kBlue, r.kGreen+2, r.kOrange-2]
 
 inputFiles = []
 legends     = ["tritrig",
-               "simp 100,60,33.3",
-               "simp 130,78,43.3",
-               "simp 100,40,30",
-               "simp 50,30,16.7"]
-outdir     = "tritrig_simp_recon_ctau10mm_anaVtxPresel_simpTightL1L1NoSharedL0_norm"#_TruthRadMatch_norm"
+               "wab",
+               "simp_100_60_33p3",
+               "simp_130_78_43p3"]
+#               "simp_100_40_30",
+#               "simp_50_30_16p7"]
+outdir     = "tritrig_wab_simp_beam_recon_ctau10mm_anaVtxPresel_norm"
 
 if not os.path.exists(outdir):
     os.makedirs(outdir)
@@ -35,20 +37,35 @@ for ifile in inFileList:
     pass
 
 canvs = []
-for key in inputFiles[0].Get('vtxana_simpTightL1L1NoSharedL0').GetListOfKeys():
+
+for key in inputFiles[0].Get('vtxana_vtxSelection').GetListOfKeys():
     histos = []
+    histos2D = []
+    xtitle = []
+    ytitle = []
+    name   = []
+
     print key.GetName()
     c = r.TCanvas()
     for i_f in range(0,len(inputFiles)):
-        histos.append(inputFiles[i_f].Get('vtxana_simpTightL1L1NoSharedL0').Get(key.GetName()))
-        histos[i_f].SetMarkerColor(colors[i_f])
-        histos[i_f].SetLineColor(colors[i_f])
+
+        if '_hh' in key.GetName():
+            histos2D.append(inputFiles[i_f].Get('vtxana_vtxSelection').Get(key.GetName()))
+            xtitle.append(histos2D[i_f].GetXaxis().GetTitle())
+            ytitle.append(histos2D[i_f].GetYaxis().GetTitle())
+            name.append(key.GetName()+'_'+legends[i_f])
+        
+        histos.append(inputFiles[i_f].Get('vtxana_vtxSelection').Get(key.GetName()))
+        #histos[i_f].SetMarkerColor(colors[i_f])
+        #histos[i_f].SetLineColor(colors[i_f])
         pass
+
+    canvs.append(utils.Make2DPlots(name,outdir,histos2D,xtitle,ytitle))
     canvs.append(utils.MakePlot(key.GetName(),outdir,histos,legends,".png",LogY=False,RatioType="Sequential",Normalise=True))
     canvs.append(utils.MakePlot(key.GetName()+"_log",outdir,histos,legends,".png",LogY=True,RatioType="Sequential",Normalise=True))
     pass
 
-outF = r.TFile("tritrig_simp_recon_ctau10mm_anaVtxPresel_simpTightL1L1NoSharedL0_norm.root","RECREATE")
+outF = r.TFile("tritrig_wab_simp_beam_recon_ctau10mm_anaVtxPresel_norm.root","RECREATE")
 outF.cd()
 for canv in canvs: canv.Write()
 outF.Close()
