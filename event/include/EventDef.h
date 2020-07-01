b1e284ddd56c8ebed3d1fc50b50d729295d8f8b8
omar-moreno <email@omarmoreno.net>
2020-06-30 23:25:54 -0700

    typedef defining a bus passenger.

--- event/include/EventDef.h
+++ event/include/EventDef.h
@@ -1,4 +1,4 @@
-// Includes for root dictionary creation
+
 #include "CalCluster.h"
 #include "CalHit.h"
 #include "Event.h"
@@ -16,3 +16,26 @@
 #include "MCTrackerHit.h"
 #include "MCEcalHit.h"
 #include "RawSvtHit.h"
+
+#include <variant>
+
+/**
+ * @type Collection
+ */
+typedef std::variant <
+    EventHeader, 
+    std::vector< CalCluster >,
+    std::vector< CalHit >,
+    std::vector< HodoCluster >, 
+    std::vector< HodoHit >, 
+    std::vector< TriggerData >, 
+    std::vector< VTPData >, 
+    std::vector< TSData >, 
+    std::vector< Particle >, 
+    std::vector< Track >, 
+    std::vector< MCTrackerHit >, 
+    std::vector< MCEcalHit >, 
+    std::vector< RawSvtHit >,
+    std::vector< Vertex >, 
+    std::map< int, MCParticle > 
+> Collection;
