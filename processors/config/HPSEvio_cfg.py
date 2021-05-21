313589c604a40946690c5b903b0ee84d175fe63b
Emrys Peets <emryspeets@gmail.com>
2021-05-21 13:47:12 -0700

    got bare evio processor running

---
+++ processors/config/HPSEvio_cfg.py
@@ -0,0 +1,41 @@
+import HpstrConf
+import sys
+import baseConfig
+
+args = baseConfig.parser.parse_args()
+
+# Use the input file to set the output file name
+in_file = args.inFilename[0]
+out_file   = args.outFilename[0]
+
+print('In file: %s' % in_file)
+print('Out file: %s' % out_file)
+
+p = HpstrConf.Process()
+
+p.run_mode = 3
+#p.max_events = 1000
+
+# Library containing processors
+p.add_library("libprocessors")
+
+###############################
+#          Processors         #
+###############################
+
+evio = HpstrConf.Processor('evio', 'HPSEvioProcessor')
+
+###############################
+#   Processor Configuration   #
+###############################
+
+#evio
+evio.parameters["debug"] = 1
+
+# Sequence which the processors will run.
+p.sequence = [evio]
+
+p.input_files=[in_file]
+p.output_files = [out_file]
+
+p.printProcess()
