362c7437019f8529fd278127b2311074c6ce5afc
Cameron Bravo <bravo@slac.stanford.edu>
2021-06-11 20:18:24 -0700

    Add possibility for 2 fits to a raw svt hit

--- event/include/RawSvtHit.h
+++ event/include/RawSvtHit.h
@@ -31,8 +31,11 @@ class RawSvtHit : public TObject {
         /** Reset the Hit object. */
         void Clear();
 
+        /** Set the fit multi */
+        void setFitN(int fitN);
+
         /** Set the fit parameters */
-        void setFit(double fit[5]);
+        void setFit(double fit[5], int fitI);
 
         /** Set the adc values */
         void setADCs(int adcs[6]);
@@ -58,8 +61,11 @@ class RawSvtHit : public TObject {
         /** Set the strip */
         void setStrip(int strip);
 
+        /** Get the fit multi */
+        int getFitN() {return fitN_;}
+
         /** Get the fit paramters */
-        double * getFit();
+        double * getFit(int fitI);
 
         /** Get the adc values */
         int * getADCs();
@@ -86,21 +92,21 @@ class RawSvtHit : public TObject {
         int getStrip();
 
         /** Get the t0 fit parameter */
-        double getT0() {return fit_[0];}
+        double getT0(int fitI) {return fit_[fitI][0];}
 
         /** Get the t0 err fit parameter */
-        double getT0err() {return fit_[1];}
+        double getT0err(int fitI) {return fit_[fitI][1];}
 
         /** Get the amplitude fit parameter */
-        double getAmp() {return fit_[2];}
+        double getAmp(int fitI) {return fit_[fitI][2];}
 
         /** Get the amplitude error fit parameter */
-        double getAmpErr() {return fit_[3];}
+        double getAmpErr(int fitI) {return fit_[fitI][3];}
 
         /** Get the chiSq probability */
-        double getChiSq() {return fit_[4];}
+        double getChiSq(int fitI) {return fit_[fitI][4];}
 
-        ClassDef(RawSvtHit, 1);	
+        ClassDef(RawSvtHit, 1);
 
     private:
 
@@ -115,7 +121,8 @@ class RawSvtHit : public TObject {
         int side_{-999}; 
         int strip_{-999}; 
         /** The fit parameters of the hit. */
-        double fit_[5]{-999.9,-999.9,-999.9,-999.9,-999.9}; 
+        int fitN_{0}; 
+        double fit_[2][5]{-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9,-999.9}; 
 
 }; // RawSvtHit
 
