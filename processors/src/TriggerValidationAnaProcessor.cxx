9ebd03db2b3f3600ec8f707b790e21091b715cab
tongtongcao <tongtongcao1@gmail.com>
2021-09-07 23:03:52 -0400

    update 4

--- processors/src/TriggerValidationAnaProcessor.cxx
+++ processors/src/TriggerValidationAnaProcessor.cxx
@@ -326,11 +326,13 @@ bool TriggerValidationAnaProcessor::process(IEvent* ievent) {
 						if(energyDiffEcalClusterVTPCluster > 1.5){
 							histos->Fill2DHisto("x_vs_y_single2_fail_energyDiffLargerthan1pt5_hh", ix, iy, weight);
 							histos->Fill2DHisto("energyGTPCluster_vs_energyEcalCluster_single2_fail_energyDiffLargerthan1pt5_hh", energyEcalCluster, energyVTPCluster, weight);
+							histos->Fill2DHisto("timeGTPCluster_vs_timeEcalCluster_single2_fail_energyDiffLargerthan1pt5_hh", timeEcalCluster, timeVTPCluster, weight);
 						}
 
 						if(energyDiffEcalClusterVTPCluster < 0){
 							histos->Fill2DHisto("x_vs_y_single2_fail_energyDiffLessthan0_hh", ix, iy, weight);
 							histos->Fill2DHisto("energyGTPCluster_vs_energyEcalCluster_single2_fail_energyDiffLessthan0_hh", energyEcalCluster, energyVTPCluster, weight);
+							histos->Fill2DHisto("timeGTPCluster_vs_timeEcalCluster_single2_fail_energyDiffLessthan0_hh", timeEcalCluster, timeVTPCluster, weight);
 						}
 
 						if(flagSingle3Top) failSingle2PassSinge3Top++;
@@ -401,11 +403,13 @@ bool TriggerValidationAnaProcessor::process(IEvent* ievent) {
 						if(energyDiffEcalClusterVTPCluster > 1.5){
 							histos->Fill2DHisto("x_vs_y_single2_fail_energyDiffLargerthan1pt5_hh", ix, iy, weight);
 							histos->Fill2DHisto("energyGTPCluster_vs_energyEcalCluster_single2_fail_energyDiffLargerthan1pt5_hh", energyEcalCluster, energyVTPCluster, weight);
+							histos->Fill2DHisto("timeGTPCluster_vs_timeEcalCluster_single2_fail_energyDiffLargerthan1pt5_hh", timeEcalCluster, timeVTPCluster, weight);
 						}
 
 						if(energyDiffEcalClusterVTPCluster < 0){
 							histos->Fill2DHisto("x_vs_y_single2_fail_energyDiffLessthan0_hh", ix, iy, weight);
 							histos->Fill2DHisto("energyGTPCluster_vs_energyEcalCluster_single2_fail_energyDiffLessthan0_hh", energyEcalCluster, energyVTPCluster, weight);
+							histos->Fill2DHisto("timeGTPCluster_vs_timeEcalCluster_single2_fail_energyDiffLessthan0_hh", timeEcalCluster, timeVTPCluster, weight);
 						}
 
 						if(flagSingle3Bot) failSingle2PassSinge3Bot++;
