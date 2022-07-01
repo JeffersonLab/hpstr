6e62c417ae9435ab494774866a3fa9627b9b2515
O'Dwyer <rodwyer1@sdf-login02.slac.stanford.edu>
2022-07-01 15:41:30 -0700

    being safe`

--- processors/src/SvtRawDataAnaProcessor.cxx
+++ processors/src/SvtRawDataAnaProcessor.cxx
@@ -61,8 +61,8 @@ void SvtRawDataAnaProcessor::initialize(TTree* tree) {
 
 bool SvtRawDataAnaProcessor::process(IEvent* ievent) {
     //std::cout<<"hello5"<<std::endl;
-    Float_t TimeRef=TimeRef_;
-    Float_t AmpRef=AmpRef_;
+    Float_t TimeRef=0.0;
+    Float_t AmpRef=0.0;
     double weight = 1.;
     for (unsigned int i_reg = 0; i_reg < regionSelections_.size(); i_reg++) 
     {
@@ -73,26 +73,32 @@ bool SvtRawDataAnaProcessor::process(IEvent* ievent) {
                 if(!(reg_selectors_[regions_[i_reg]]->passCutEq("getN_et",getNum,weight))){continue;}
                 if(!(reg_selectors_[regions_[i_reg]]->passCutLt("getId_lt",J,weight))){continue;} 
                 if(!(reg_selectors_[regions_[i_reg]]->passCutGt("getId_gt",J,weight))){continue;}
+                //std::cout<<"hellO"<<std::endl;           
                 if(!(reg_selectors_[regions_[i_reg]]->passCutLt("chi_lt",thisHit->getChiSq(J),weight))){continue;}
                 if(!(reg_selectors_[regions_[i_reg]]->passCutGt("chi_gt",thisHit->getChiSq(J),weight))){continue;}
                 if(reg_selectors_[regions_[i_reg]]->passCutEq("doing_ct",1.0,weight)){
                     if(!(std::abs((thisHit->getT0(J))-TimeRef)<std::abs(thisHit->getT0((J+1)%2)-TimeRef))){continue;}          
                 }
-                if(reg_selectors_[regions_[i_reg]]->passCutEq("doing_ft",1.0,weight)){
-                    if((std::abs((thisHit->getT0(J))-TimeRef)<std::abs(thisHit->getT0((J+1)%2)-TimeRef))){continue;}          
-                }
-                 if(reg_selectors_[regions_[i_reg]]->passCutEq("doing_sa",1.0,weight)){
-                    if(!(std::abs((thisHit->getAmp(J))-AmpRef)<std::abs(thisHit->getAmp((J+1)%2)-AmpRef))){continue;}          
-                }
-                if(reg_selectors_[regions_[i_reg]]->passCutEq("doing_la",1.0,weight)){
-                    if((std::abs((thisHit->getAmp(J))-AmpRef)<std::abs(thisHit->getAmp((J+1)%2)-AmpRef))){continue;}          
-                }
+                //std::cout<<"hellO2"<<std::endl;
+                //if(reg_selectors_[regions_[i_reg]]->passCutEq("doing_ft",1.0,weight)){
+                //    if((std::abs((thisHit->getT0(J))-TimeRef)<std::abs(thisHit->getT0((J+1)%2)-TimeRef))){std::cout<<"hellO2"<<std::endl;continue;}          
+                //}
+                //std::cout<<"hellO3"<<std::endl;
+                // if(reg_selectors_[regions_[i_reg]]->passCutEq("doing_sa",1.0,weight)){
+                //    if(!(std::abs((thisHit->getAmp(J))-AmpRef)<std::abs(thisHit->getAmp((J+1)%2)-AmpRef))){continue;}          
+                //}
+                //std::cout<<"hellO4"<<std::endl;
+                //if(reg_selectors_[regions_[i_reg]]->passCutEq("doing_la",1.0,weight)){
+                //    if((std::abs((thisHit->getAmp(J))-AmpRef)<std::abs(thisHit->getAmp((J+1)%2)-AmpRef))){continue;}          
+                //}
+                //std::cout<<"hellO2"<<std::endl;
                 Float_t TimeDiff=-42069.0;
                 Float_t AmpDiff=-42069.0;
                 if(getNum==2){
                     TimeDiff=(thisHit->getT0(J))-(thisHit->getT0((J+1)%2));
                     AmpDiff=(thisHit->getT0(J))-(thisHit->getT0((J+1)%2)); 
                 }
+                //std::cout<<"I GOT HERE"<<std::endl;
                 reg_histos_[regions_[i_reg]]->FillHistograms(thisHit,weight,J,i,TimeDiff,AmpDiff);
             }
         }
