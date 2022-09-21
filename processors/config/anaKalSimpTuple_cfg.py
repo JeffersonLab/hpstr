459b3083d300361de3b4de540ec9369490835538
Alic Spellman <alspellm@ucsc.edu>
2022-09-21 13:28:38 -0700

    removing files not meant to be changed from current master

--- processors/config/anaKalSimpTuple_cfg.py
+++ processors/config/anaKalSimpTuple_cfg.py
@@ -3,16 +3,17 @@ import sys
 import os
 import baseConfig as base
 
+
 base.parser.add_argument("-w", "--tracking", type=str, dest="tracking",
                   help="Which tracking to use to make plots", metavar="tracking", default="KF")
 options = base.parser.parse_args()
+
 
 # Use the input file to set the output file name
 infile = options.inFilename
 outfile = options.outFilename
 
-#outfile = outfile.split(".root")[0]+"_"+options.tracking+".root"
-outfile = outfile.split(".root")[0]+".root"
+outfile = outfile.split(".root")[0]+"_"+options.tracking+".root"
 
 print('Input file: %s' % infile)
 print('Output file: %s' % outfile)
@@ -41,11 +42,10 @@ recoana_kf.parameters["trkColl"] = "KalmanFullTracks"
 recoana_kf.parameters["tsColl"] = "TSData"
 recoana_kf.parameters["vtxColl"] = "UnconstrainedV0Vertices_KF"
 recoana_kf.parameters["mcColl"]  = "MCParticle"
-recoana_kf.parameters["hitColl"] = "SiClusters"
-recoana_kf.parameters["vtxSelectionjson"] = os.environ['HPSTR_BASE']+'/analysis/selections/vertexSelection_2019.json'
-recoana_kf.parameters["histoCfg"] = os.environ['HPSTR_BASE']+"/analysis/plotconfigs/tracking/vtxAnalysis_2019.json"
+recoana_kf.parameters["hitColl"] = "SiClustersOnTrack"
+recoana_kf.parameters["vtxSelectionjson"] = os.environ['HPSTR_BASE']+'/analysis/selections/vertexSelection_8hit.json'
+recoana_kf.parameters["histoCfg"] = os.environ['HPSTR_BASE']+"/analysis/plotconfigs/tracking/simpAnalysis_2016.json"
 recoana_kf.parameters["mcHistoCfg"] = os.environ['HPSTR_BASE']+'/analysis/plotconfigs/mc/basicMC.json'
-#####
 recoana_kf.parameters["beamE"] = base.beamE[str(options.year)]
 recoana_kf.parameters["isData"] = options.isData
 recoana_kf.parameters["analysis"] = options.analysis
@@ -62,12 +62,15 @@ elif (options.isData==0):
 else:
     print("Specify which type of ntuple you are running on: -t 1 [for Data] / -t 0 [for MC]")
 
+
 recoana_kf.parameters["CalTimeOffset"]=CalTimeOffset
 #Region definitions
 
 RegionPath=os.environ['HPSTR_BASE']+"/analysis/selections/"
-recoana_kf.parameters["regionDefinitions"] = [RegionPath+'Tight_2019.json',
-                                              RegionPath+'radMatchTight_2019.json']
+recoana_kf.parameters["regionDefinitions"] = [RegionPath+'simpTight.json',
+                                              RegionPath+'simpTightL1L1.json']
+                                              #RegionPath+'simpTightL1L1NoSharedL0.json']
+
 #RecoHitAna
 recoana_gbl.parameters = recoana_kf.parameters.copy()
 recoana_gbl.parameters["anaName"] = "vtxana_gbl"
@@ -75,6 +78,7 @@ recoana_gbl.parameters["vtxColl"] = "UnconstrainedV0Vertices"
 recoana_gbl.parameters["tsColl"]   = "TSData"
 recoana_gbl.parameters["hitColl"] = "RotatedHelicalOnTrackHits"
 recoana_gbl.parameters["trkColl"] = "GBLTracks"
+
 
 #MCParticleAna
 mcana.parameters["debug"] = 0
