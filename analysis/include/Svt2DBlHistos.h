734375c54902c1c92bfd7333aa1ec75609e158f4
Alic Spellman <alspellm@ucsc.edu>
2023-01-05 10:11:28 -0800

    added more doxygen comments

--- analysis/include/Svt2DBlHistos.h
+++ analysis/include/Svt2DBlHistos.h
@@ -14,27 +14,45 @@
 
 #include <string>
 
-
+/**
+ * @brief description
+ *
+ * details
+ */
 class Svt2DBlHistos : public HistoManager{
 
     public:
+        /**
+         * @brief Constructor
+         *
+         * @param inputName
+         * @param mmapper_
+         */
         Svt2DBlHistos(const std::string& inputName, ModuleMapper* mmapper_);
         ~Svt2DBlHistos();
 
+        /** 
+         * @brief description
+         *
+         */
         void DefineHistos();
+
+        /**
+         * @brief description
+         *
+         * @param rawSvtHits_
+         * @param weight
+         */
         void FillHistograms(std::vector<RawSvtHit*> *rawSvtHits_,float weight = 1.);
 
 
     private:
 
-        int Event_number=0;
-        int debug_ = 1;
+        int Event_number=0; //!< description
+        int debug_ = 1 //!< description;
 
-        TH1F* svtCondHisto{nullptr};  
+        TH1F* svtCondHisto{nullptr} //!< description;  
         //ModuleMapper
-        ModuleMapper* mmapper_;
-
+        ModuleMapper* mmapper_ //!< description;
 };
-
-
 #endif
--- processors/include/SvtBlFitHistoProcessor.h
+++ processors/include/SvtBlFitHistoProcessor.h
@@ -13,57 +13,100 @@
 #include "HistoManager.h"
 #include "FlatTupleMaker.h"
 
+/**
+ * @brief Insert description here.
+ * more details
+ */
 class SvtBlFitHistoProcessor : public Processor {
 
     public: 
 
+        /**
+￼         * @brief Constructor
+￼         * 
+￼         * @param name 
+￼         * @param process 
+￼         */
         SvtBlFitHistoProcessor(const std::string& name, Process& process);
         
         ~SvtBlFitHistoProcessor();
 
+        /**
+￼         * @brief description
+￼         * 
+￼         * @param parameters 
+￼         */
         virtual void configure(const ParameterSet& parameters);
 
+        /**
+￼         * @brief description
+￼         * 
+￼         * @param inFilename 
+￼         * @param outFilename 
+￼         */
         virtual void initialize(std::string inFilename, std::string outFilename);
 
+        /**
+￼         * @brief description
+￼         * 
+￼         * @return true 
+￼         * @return false 
+￼         */
         virtual bool process();
 
+        /**
+￼         * @brief description
+￼         * 
+￼         * @param tree 
+￼         */
         virtual void initialize(TTree* tree) {};
 
+        /**
+￼         * @brief description
+￼         * 
+￼         * @param event 
+￼         * @return true 
+￼         * @return false 
+￼         */
         virtual bool process(IEvent* event) {};
 
+        /**
+￼         * @brief description
+￼         * 
+￼         */
         virtual void finalize();
 
     private:
 
-        TFile* inF_{nullptr};
-        TFile* outF_chi2{nullptr};
+        TFile* inF_{nullptr}; //!< description
+        TFile* outF_chi2{nullptr} //!< description;
 
-        int  year_=2019;
+        int  year_=2019 //!< description;
 
         //Select which layer to fit baselines. Default is all.
-        std::string layer_{""};
+        std::string layer_{""} //!< description;
 
         //json file for histo config
-        std::string histCfgFilename_;
+        std::string histCfgFilename_; //!< description
 
         //json file for reading in rawsvthit histograms
-        std::string rawhitsHistCfgFilename_;
+        std::string rawhitsHistCfgFilename_; //!< description
 
         //Histogram handlers
-        BlFitHistos* fitHistos_{nullptr};
+        BlFitHistos* fitHistos_{nullptr};//!< description
 
         //Load apv channel thresholds in
-        std::string thresholdsFileIn_;
+        std::string thresholdsFileIn_;//!< description
 
         //configurable parameters for fitting. All have default settings.
-        int rebin_{};
-        int minStats_{};
-        int deadRMS_{};
-        int debug_{0};
+        int rebin_{};//!< description
+        int minStats_{};//!< description
+        int deadRMS_{};//!< description
+        int debug_{0};//!< description
 
-        std::string simpleGausFit_;
+        std::string simpleGausFit_;//!< description
 
-        FlatTupleMaker* flat_tuple_{nullptr};
+        FlatTupleMaker* flat_tuple_{nullptr};//!< description
 
 
 };
