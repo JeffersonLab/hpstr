ee9da59815aeca070c25480a7afb5c0f63aeb26b
Abhisek Datta <abhisek.datta@cern.ch>
2023-09-30 14:20:53 -0700

    bug fix

--- analysis/src/SimPartHistos.cxx
+++ analysis/src/SimPartHistos.cxx
@@ -2,11 +2,11 @@
 #include <math.h>
 
 void SimPartHistos::FillAcceptance(std::vector<MCParticle*> *MCParticles_, std::vector<Track*> *RecoTracks_, std::vector<TrackerHit*> *RecoTrackerClusters_, std::vector<CalCluster*> *RecoEcalClusters_, float weight) {
-    int nParts = mcParts->size();
+    int nParts = MCParticles_->size();
     Fill1DHisto("numMCparts_h", (float)nParts, weight);
 
     for (int i=0; i<nParts; i++) {
-        MCParticle *part = mcParts->at(i);
+        MCParticle *part = MCParticles_->at(i);
         int pdg = part->getPDG();
         int gen = part->getGenStatus();
         std::vector<double> momentum_V = part->getMomentum();
