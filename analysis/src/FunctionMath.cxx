a8544526c5e4d31ff9b80f4baa12ef63fc332fe0
EmrysPeets <epeets@stanford.edu>
2023-11-28 08:00:12 -0800

    Global fitting changes made and functions have been added.

--- analysis/src/FunctionMath.cxx
+++ analysis/src/FunctionMath.cxx
@@ -27,6 +27,13 @@ double FunctionMath::LegendreFunction(double x, double* p, int order) {
     return total;
 }
 
+
+double FunctionMath::las3pluslas6_Function(double x, double* p) {
+
+((TMath::Erf((x-p[1])/p[0])+1)/2 * p[2]*TMath::Power((1-x),p[3])*TMath::Exp(p[4]*log(x)))+((TMath::Erf((x-p[6])/p[5])+1)/2 * p[7]*TMath::Power((1-x),p[8])*TMath::Power(1+x,p[9]*x));
+
+}    
+
 double FunctionMath::Gaussian(double x, double amplitude, double mean, double stddev) {
     return amplitude * 1.0 / (sqrt(2.0 * TMath::Pi() * pow(stddev, 2))) * TMath::Exp(-pow((x - mean), 2) / (2.0 * pow(stddev, 2)));
 }
