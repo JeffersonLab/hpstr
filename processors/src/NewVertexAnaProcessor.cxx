23cc21717c9cca86718c88922d0214b7741176e5
tomeichlersmith <eichl008@umn.edu>
2024-03-22 13:01:41 -0400

    add InvM histogram calculated from ele and pos directly
    introduce TrackSmearingTool into NewVertexAnaProcessor in same way it is
    used in TrackingAnaProcessor, compiles and runs without a smearing file
    provided (i.e. no effect as intended)

--- processors/src/NewVertexAnaProcessor.cxx
+++ processors/src/NewVertexAnaProcessor.cxx
@@ -38,6 +38,8 @@ void NewVertexAnaProcessor::configure(const ParameterSet& parameters) {
         isData_  = parameters.getInteger("isData",isData_);
         analysis_        = parameters.getString("analysis");
 
+        pSmearingFile_ = parameters.getString("pSmearingFile",pSmearingFile_);
+
         //region definitions
         regionSelections_ = parameters.getVString("regionDefinitions",regionSelections_);
 
@@ -244,6 +246,11 @@ void NewVertexAnaProcessor::initialize(TTree* tree) {
     //tree_->SetBranchAddress(hitColl_.c_str(), &hits_   , &bhits_);
     if (brMap_.find(hitColl_.c_str()) != brMap_.end()) tree_->SetBranchAddress(hitColl_.c_str(), &hits_ , &bhits_);
     if(!isData_ && !mcColl_.empty()) tree_->SetBranchAddress(mcColl_.c_str() , &mcParts_, &bmcParts_);
+
+    if (not pSmearingFile_.empty()) {
+      // just using the same seed=42 for now
+      smearingTool_    =   std::make_shared<TrackSmearingTool>(pSmearingFile_,false);
+    }
 }
 
 bool NewVertexAnaProcessor::process(IEvent* ievent) {
@@ -349,6 +356,11 @@ bool NewVertexAnaProcessor::process(IEvent* ievent) {
         //Track Time Corrections
         ele_trk.applyCorrection("track_time",eleTrackTimeBias_);
         pos_trk.applyCorrection("track_time", posTrackTimeBias_);
+
+        if (smearingTool_) {
+          smearingTool_->updateWithSmearP(ele_trk);
+          smearingTool_->updateWithSmearP(pos_trk);
+        }
 
         //Add the momenta to the tracks - do not do that
         //ele_trk.setMomentum(ele->getMomentum()[0],ele->getMomentum()[1],ele->getMomentum()[2]);
@@ -530,6 +542,7 @@ bool NewVertexAnaProcessor::process(IEvent* ievent) {
         _vtx_histos->Fill1DHisto("pos_track_n2dhits_h", pos2dHits, weight);
         _vtx_histos->Fill1DHisto("vtx_Psum_h", p_ele.P()+p_pos.P(), weight);
         _vtx_histos->Fill1DHisto("vtx_Esum_h", ele_E + pos_E, weight);
+        _vtx_histos->Fill1DHisto("ele_pos_InvM_h", (p_ele+p_pos).M(), weight);
         _vtx_histos->Fill1DHisto("ele_pos_clusTimeDiff_h", (corr_eleClusterTime - corr_posClusterTime), weight);
         _vtx_histos->Fill2DHisto("ele_vtxZ_iso_hh", TMath::Min(ele_trk.getIsolation(0), ele_trk.getIsolation(1)), vtx->getZ(), weight);
         _vtx_histos->Fill2DHisto("pos_vtxZ_iso_hh", TMath::Min(pos_trk.getIsolation(0), pos_trk.getIsolation(1)), vtx->getZ(), weight);
@@ -640,6 +653,11 @@ bool NewVertexAnaProcessor::process(IEvent* ievent) {
             //Track Time Corrections
             ele_trk.applyCorrection("track_time",eleTrackTimeBias_);
             pos_trk.applyCorrection("track_time", posTrackTimeBias_);
+    
+            if (smearingTool_) {
+              smearingTool_->updateWithSmearP(ele_trk);
+              smearingTool_->updateWithSmearP(pos_trk);
+            }
 
             //Add the momenta to the tracks
             //ele_trk.setMomentum(ele->getMomentum()[0],ele->getMomentum()[1],ele->getMomentum()[2]);
@@ -1026,6 +1044,10 @@ bool NewVertexAnaProcessor::process(IEvent* ievent) {
             //Track Time Corrections
             ele_trk.applyCorrection("track_time",eleTrackTimeBias_);
             pos_trk.applyCorrection("track_time", posTrackTimeBias_);
+            if (smearingTool_) {
+              smearingTool_->updateWithSmearP(ele_trk);
+              smearingTool_->updateWithSmearP(pos_trk);
+            }
 
             //Get the layers hit on each track
             std::vector<int> ele_hit_layers = ele_trk.getHitLayers();
@@ -1147,6 +1169,7 @@ bool NewVertexAnaProcessor::process(IEvent* ievent) {
             _reg_vtx_histos[region]->Fill1DHisto("pos_track_n2dhits_h", pos2dHits, weight);
             _reg_vtx_histos[region]->Fill1DHisto("vtx_Psum_h", p_ele.P()+p_pos.P(), weight);
             _reg_vtx_histos[region]->Fill1DHisto("vtx_Esum_h", eleClus.getEnergy()+posClus.getEnergy(), weight);
+            _reg_vtx_histos[region]->Fill1DHisto("vtx_InvM_h", (p_ele+p_pos).M(), weight);
             _reg_vtx_histos[region]->Fill2DHisto("ele_vtxZ_iso_hh", TMath::Min(ele_trk.getIsolation(0), ele_trk.getIsolation(1)), vtx->getZ(), weight);
             _reg_vtx_histos[region]->Fill2DHisto("pos_vtxZ_iso_hh", TMath::Min(pos_trk.getIsolation(0), pos_trk.getIsolation(1)), vtx->getZ(), weight);
             _reg_vtx_histos[region]->Fill2DTrack(&ele_trk,weight,"ele_");
