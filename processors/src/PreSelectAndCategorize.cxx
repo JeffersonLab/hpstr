a996ca3d48122fb8bfda553216225787320a43e6
tomeichlersmith <eichl008@umn.edu>
2024-06-06 11:04:04 -0700

    BEFORE selecting events, pre-select "quality" vertices
    this means we effectively have two cutflows, one selecting vertices and one selecting events, both are stored separately

--- processors/src/PreSelectAndCategorize.cxx
+++ processors/src/PreSelectAndCategorize.cxx
@@ -36,11 +36,14 @@
 
 
 class Cutflow {
+  std::string name_, nocut_name_;
   std::unique_ptr<TH1F> h_cutflow_;
   std::map<std::string,std::pair<std::size_t,std::unique_ptr<TH1F>>> cuts_;
   bool keep_;
   std::vector<std::optional<bool>> cut_desc_;
  public:
+  Cutflow(const std::string& name, const std::string& nocut_name)
+    : name_{name}, nocut_name_{nocut_name} {}
   void add(const std::string& name, int nbins, float min, float max) {
     if (h_cutflow_) {
       throw std::runtime_error("Cannot add more cuts after init");
@@ -50,21 +53,27 @@ class Cutflow {
                 (name+" N-1").c_str(),
                 nbins, min, max
             );
-    cuts_.emplace(name, std::make_pair<std::size_t, std::unique_ptr<TH1F>>(cuts_.size(), std::move(h)));
+    cuts_.emplace(
+        name,
+        std::make_pair<std::size_t, std::unique_ptr<TH1F>>(
+          cuts_.size(),
+          std::move(h)
+        )
+    );
   }
   void init() {
     if (h_cutflow_) {
       throw std::runtime_error("Cannot init twice.");
     }
     h_cutflow_ = std::make_unique<TH1F>(
-        "cutflow_h",
-        "Cutflow",
+        (name_+"_cutflow_h").c_str(),
+        (name_+"Cutflow").c_str(),
         cuts_.size()+1, -1.5, cuts_.size()-0.5
     );
     cut_desc_.resize(cuts_.size());
   }
   void begin_event() {
-    h_cutflow_->Fill("readout", 1.);
+    h_cutflow_->Fill(nocut_name_.c_str(), 1.);
     keep_ = true;
     for (auto& d : cut_desc_) d.reset();
   }
@@ -159,7 +168,8 @@ class PreSelectAndCategorize : public Processor {
   double posTrackTimeBias_{0.0};
   double calTimeOffset_{0.0};
 
-  Cutflow cf_;
+  Cutflow vertex_cf_{"vertex","reconstructed"};
+  Cutflow event_cf_{"event","readout"};
   std::unique_ptr<TH1F> n_vertices_h_;
   std::shared_ptr<TrackSmearingTool> smearingTool_;
   std::shared_ptr<AnaHelpers> _ah; //!< description
@@ -211,28 +221,30 @@ void PreSelectAndCategorize::initialize(TTree* tree) {
   bus_.board_input<std::vector<Vertex*>>(tree, vtxColl_);
   if(not isData_ and not mcColl_.empty())
     bus_.board_input<std::vector<MCParticle*>>(tree, mcColl_);
+  /* pre-selection on vertices */
+  vertex_cf_.add("abs_ele_track_before_6ns", 120, 0.0, 24.0);
+  vertex_cf_.add("abs_pos_track_before_6ns", 120, 0.0, 24.0);
+  vertex_cf_.add("clusters_within_1.45ns", 100,0.0,10.0);
+  vertex_cf_.add("ele_track_cluster_within_4ns", 80, 0.0, 16.0);
+  vertex_cf_.add("pos_track_cluster_within_4ns", 80, 0.0, 16.0);
+  vertex_cf_.add("ele_track_chi2ndf", 100, 0.0, 20.0);
+  vertex_cf_.add("pos_track_chi2ndf", 100, 0.0, 20.0);
+  vertex_cf_.add("electron_below_1.75GeV", 230,0.0,2.3);
+  vertex_cf_.add("ele_min_8_hits", 14, 0, 14);
+  vertex_cf_.add("pos_min_8_hits", 14, 0, 14);
+  vertex_cf_.add("vertex_chi2", 100,0.0,50.0);
+  vertex_cf_.add("psum_lt_2.4GeV", 300, 0.0, 3.0);
+  vertex_cf_.init();
   
-  cf_.add("pair1trigger", 2, -0.5, 1.5);
-  cf_.add("at_least_one_vertex", 10, 0.0, 10.0);
-  cf_.add("no_extra_vertices", 10, 0.0, 10.0);
-  /* further selection on vertices
-  cf_.add("clusters_within_1.45ns", 100,0.0,10.0);
-  cf_.add("electron_below_1.75GeV", 230,0.0,2.3);
-  cf_.add("vertex_chi2", 100,0.0,50.0);
-  cf_.add("ele_track_chi2ndf", 100, 0.0, 20.0);
-  cf_.add("pos_track_chi2ndf", 100, 0.0, 20.0);
-  cf_.add("ele_min_8_hits", 14, 0, 14);
-  cf_.add("pos_min_8_hits", 14, 0, 14);
-  cf_.add("ele_track_before_6ns", 120, 0.0, 24.0);
-  cf_.add("pos_track_before_6ns", 120, 0.0, 24.0);
-  cf_.add("ele_track_cluster_within_4ns", 80, 0.0, 16.0);
-  cf_.add("pos_track_cluster_within_4ns", 80, 0.0, 16.0);
-  */
-  cf_.init();
+  /* event selection after vertex selection */
+  event_cf_.add("pair1trigger", 2, -0.5, 1.5);
+  event_cf_.add("at_least_one_vertex", 10, 0.0, 10.0);
+  event_cf_.add("no_extra_vertices", 10, 0.0, 10.0);
+  event_cf_.init();
 
   n_vertices_h_ = std::make_unique<TH1F>(
       "n_vertices_h",
-      "N Vertices in Event (no selection)",
+      "N Vertices in Event (preselected)",
       10,-0.5,9.5
   );
 }
@@ -271,97 +283,165 @@ void PreSelectAndCategorize::setFile(TFile* out_file) {
 bool PreSelectAndCategorize::process(IEvent*) {
   const auto& eh{bus_.get<EventHeader>("EventHeader")};
   int run_number = eh.getRunNumber();
-  cf_.begin_event();
+
+  event_cf_.begin_event();
 
   // re-apply trigger desicion to data sample since it contains
   // other triggers within it
   // MC is created with only this trigger AND the event header
   // is not updated so we need to skip this check for MC
-  cf_.apply("pair1trigger", (not isData_ or eh.isPair1Trigger()));
-  if (not cf_.keep()) {
+  event_cf_.apply("pair1trigger", (not isData_ or eh.isPair1Trigger()));
+  if (not event_cf_.keep()) {
+    // we leave BEFORE filling the vertex counting histogram
+    // so that the vertex count histogram is relative to this trigger
+    // and does not include other triggers
     return true;
   }
 
   const auto& vtxs{bus_.get<std::vector<Vertex*>>(vtxColl_)};
-  n_vertices_h_->Fill(vtxs.size());
-  cf_.apply("at_least_one_vertex", vtxs.size() > 0);
-  cf_.apply("no_extra_vertices", vtxs.size() < 2);
-  if (not cf_.keep()) {
+  /**
+   * pre-selection on vertices defining "quality" vertices
+   *
+   * In this process of pre-selecting vertices, we also apply corrections
+   * and momentum smearing to the underlying particle tracks and clusters.
+   * These are then stored in our new copy of these objects in the resulting
+   * preselected_vtx collection (vtx, invm_smear, ele, pos).
+   *
+   * We need to keep the particles separately since the particles "stored"
+   * within Vertex are references to the collections. We make a copy here
+   * to apply corrections and do not modify the collections referenced
+   * elsewhere in memory.
+   */
+  std::vector<std::tuple<Vertex,double,Particle,Particle>> preselected_vtx;
+  for (Vertex* vtx : vtxs) {
+    // access the indiviual Vertex, electron, and positron
+    // and add corrections to them before applying pre-selection
+    int i_ele{-1}, i_pos{-1};
+    for (int ipart = 0; ipart < vtx->getParticles().GetEntries(); ++ipart) {
+      int pdg_id = ((Particle*)vtx->getParticles().At(ipart))->getPDG();
+      if (pdg_id == 11) {
+        i_ele = ipart;
+      } else if (pdg_id == -11) {
+        i_pos = ipart;
+      }
+    }
+    if (i_ele < 0 or i_pos < 0) {
+      throw std::runtime_error("Vertex formed without either an electron or positron!");
+    }
+
+    Particle ele = *dynamic_cast<Particle*>(vtx->getParticles().At(i_ele));
+    Particle pos = *dynamic_cast<Particle*>(vtx->getParticles().At(i_pos));
+
+    // for both particles, get cluster, update, re-set cluster
+    CalCluster ele_clu = ele.getCluster();
+    CalCluster pos_clu = pos.getCluster();
+
+    ele_clu.setTime(ele_clu.getTime() - calTimeOffset_);
+    pos_clu.setTime(pos_clu.getTime() - calTimeOffset_);
+  
+    ele.setCluster(&ele_clu);
+    pos.setCluster(&pos_clu);
+  
+    Track ele_trk = ele.getTrack();
+    Track pos_trk = pos.getTrack();
+  
+    // deduce beam position corrections if config was passed
+    std::vector<double> beamPosCorrections = {0., 0., 0.};
+    if (not bpc_configs_.empty()) {
+      for (const auto& [run, entry] : bpc_configs_.items()) {
+        if (std::stoi(run) > eh.getRunNumber()) {
+          break;
+        }
+        beamPosCorrections = {
+          entry["beamspot_x"],
+          entry["beamspot_y"],
+          entry["beamspot_z"]
+        };
+      }
+    }
+    ele_trk.applyCorrection("z0", beamPosCorrections.at(1));
+    pos_trk.applyCorrection("z0", beamPosCorrections.at(1));
+  
+    // correct track timing bias
+    ele_trk.applyCorrection("track_time", eleTrackTimeBias_);
+    pos_trk.applyCorrection("track_time", posTrackTimeBias_);
+  
+    // smear track momentum
+    double invm_smear = 1.;
+    if (smearingTool_) {
+      double unsmeared_prod = ele_trk.getP()*pos_trk.getP();
+      smearingTool_->updateWithSmearP(ele_trk);
+      smearingTool_->updateWithSmearP(pos_trk);
+      double smeared_prod = ele_trk.getP()*pos_trk.getP();
+      invm_smear = sqrt(smeared_prod/unsmeared_prod);
+    }
+  
+    // put tracks back into their particles
+    // with their new data
+    ele.setTrack(&ele_trk);
+    pos.setTrack(&pos_trk);
+
+    // corrections have been applied, now onto pre-selecting
+
+    double cluster_tdiff{abs(ele.getCluster().getTime()-pos.getCluster().getTime())};
+    int ele_nhits = ele.getTrack().getTrackerHitCount();
+    if (not ele.getTrack().isKalmanTrack()) ele_nhits*=2;
+    int pos_nhits = pos.getTrack().getTrackerHitCount();
+    if (not pos.getTrack().isKalmanTrack()) pos_nhits*=2;
+    double ele_track_cluster_tdiff{
+          abs(ele.getTrack().getTrackTime()-ele.getCluster().getTime())
+    };
+    double pos_track_cluster_tdiff{
+          abs(pos.getTrack().getTrackTime()-pos.getCluster().getTime())
+    };
+
+    vertex_cf_.begin_event();
+    vertex_cf_.apply("abs_ele_track_before_6ns", abs(ele.getTrack().getTrackTime()) < 6.0);
+    vertex_cf_.apply("abs_pos_track_before_6ns", abs(pos.getTrack().getTrackTime()) < 6.0);
+    vertex_cf_.apply("clusters_within_1.45ns", cluster_tdiff < 1.45);
+    vertex_cf_.apply("ele_track_cluster_within_4ns", ele_track_cluster_tdiff < 4.0);
+    vertex_cf_.apply("pos_track_cluster_within_4ns", pos_track_cluster_tdiff < 4.0);
+    vertex_cf_.apply("ele_track_chi2ndf", ele.getTrack().getChi2Ndf() < 20.0);
+    vertex_cf_.apply("pos_track_chi2ndf", pos.getTrack().getChi2Ndf() < 20.0);
+    vertex_cf_.apply("electron_below_1.75GeV", ele.getTrack().getP() < 1.75);
+    vertex_cf_.apply("ele_min_8_hits", ele_nhits > 7);
+    vertex_cf_.apply("pos_min_8_hits", pos_nhits > 7);
+    vertex_cf_.apply("vertex_chi2", vtx->getChi2() < 20.0);
+    vertex_cf_.apply("psum_lt_2.4GeV", ele.getTrack().getP()+pos.getTrack().getP() < 2.4);
+  
+    vertex_cf_.fill_nm1("abs_ele_track_before_6ns", abs(ele.getTrack().getTrackTime()));
+    vertex_cf_.fill_nm1("abs_pos_track_before_6ns", abs(pos.getTrack().getTrackTime()));
+    vertex_cf_.fill_nm1("clusters_within_1.45ns", cluster_tdiff);
+    vertex_cf_.fill_nm1("ele_track_cluster_within_4ns", ele_track_cluster_tdiff);
+    vertex_cf_.fill_nm1("pos_track_cluster_within_4ns", pos_track_cluster_tdiff);
+    vertex_cf_.fill_nm1("ele_track_chi2ndf", ele.getTrack().getChi2Ndf());
+    vertex_cf_.fill_nm1("pos_track_chi2ndf", pos.getTrack().getChi2Ndf());
+    vertex_cf_.fill_nm1("electron_below_1.75GeV", ele.getTrack().getP());
+    vertex_cf_.fill_nm1("ele_min_8_hits", ele_nhits);
+    vertex_cf_.fill_nm1("pos_min_8_hits", pos_nhits);
+    vertex_cf_.fill_nm1("vertex_chi2", vtx->getChi2());
+    vertex_cf_.fill_nm1("psum_lt_2.4GeV", ele.getTrack().getP()+pos.getTrack().getP());
+  
+    if (vertex_cf_.keep()) {
+      preselected_vtx.emplace_back(*vtx, invm_smear, ele, pos);
+    }
+  }
+
+  n_vertices_h_->Fill(preselected_vtx.size());
+  event_cf_.apply("at_least_one_vertex", preselected_vtx.size() > 0);
+  event_cf_.apply("no_extra_vertices", preselected_vtx.size() < 2);
+  if (not event_cf_.keep()) {
     return true;
   }
   
   // correct number of vertices (i.e. only one)
-  // access the indiviual Vertex, electron, and positron
-  // and add corrections to them before serialization
-  Vertex vtx = *vtxs.at(0);
-  int i_ele{-1}, i_pos{-1};
-  for (int ipart = 0; ipart < vtx.getParticles().GetEntries(); ++ipart) {
-    int pdg_id = ((Particle*)vtx.getParticles().At(ipart))->getPDG();
-    if (pdg_id == 11) {
-      i_ele = ipart;
-    } else if (pdg_id == -11) {
-      i_pos = ipart;
-    }
-  }
-  if (i_ele < 0 or i_pos < 0) {
-    throw std::runtime_error("Vertex formed without either an electron or positron!");
-  }
-
-  Particle ele = *dynamic_cast<Particle*>(vtx.getParticles().At(i_ele));
-  Particle pos = *dynamic_cast<Particle*>(vtx.getParticles().At(i_pos));
-
-  // for both particles, get cluster, update, re-set cluster
-  CalCluster ele_clu = ele.getCluster();
-  CalCluster pos_clu = pos.getCluster();
-
-  ele_clu.setTime(ele_clu.getTime() - calTimeOffset_);
-  pos_clu.setTime(pos_clu.getTime() - calTimeOffset_);
-
-  ele.setCluster(&ele_clu);
-  pos.setCluster(&pos_clu);
-
-  Track ele_trk = ele.getTrack();
-  Track pos_trk = pos.getTrack();
-
-  // deduce beam position corrections if config was passed
-  std::vector<double> beamPosCorrections = {0., 0., 0.};
-  if (not bpc_configs_.empty()) {
-    for (const auto& [run, entry] : bpc_configs_.items()) {
-      if (std::stoi(run) > eh.getRunNumber()) {
-        break;
-      }
-      beamPosCorrections = {
-        entry["beamspot_x"],
-        entry["beamspot_y"],
-        entry["beamspot_z"]
-      };
-    }
-  }
-  ele_trk.applyCorrection("z0", beamPosCorrections.at(1));
-  pos_trk.applyCorrection("z0", beamPosCorrections.at(1));
-
-  // correct track timing bias
-  ele_trk.applyCorrection("track_time", eleTrackTimeBias_);
-  pos_trk.applyCorrection("track_time", posTrackTimeBias_);
-
-  // smear track momentum
-  double invm_smear = 1.;
-  if (smearingTool_) {
-    double unsmeared_prod = ele_trk.getP()*pos_trk.getP();
-    smearingTool_->updateWithSmearP(ele_trk);
-    smearingTool_->updateWithSmearP(pos_trk);
-    double smeared_prod = ele_trk.getP()*pos_trk.getP();
-    invm_smear = sqrt(smeared_prod/unsmeared_prod);
-  }
-
-  // put tracks back into their particles
-  // with their new data
-  ele.setTrack(&ele_trk);
-  pos.setTrack(&pos_trk);
+  // unpack the vector of vertices into the single elements
+  auto [ vtx, invm_smear, ele, pos ] = preselected_vtx.at(0);
 
   // earliest layer hit categories
   bool eleL1{false}, eleL2{false},
        posL1{false}, posL2{false};
+  Track ele_trk{ele.getTrack()}, pos_trk{pos.getTrack()};
   _ah->InnermostLayerCheck(&ele_trk, eleL1, eleL2);
   _ah->InnermostLayerCheck(&pos_trk, posL1, posL2);
   bus_.set("eleL1", eleL1);
@@ -394,50 +474,6 @@ bool PreSelectAndCategorize::process(IEvent*) {
   bus_.set("pos", pos);
 
   /**
-   * Further pre-selection on vertices.
-  double cluster_tdiff{abs(ele.getCluster().getTime()-pos.getCluster().getTime())};
-  int ele_nhits = ele.getTrack().getTrackerHitCount();
-  if (not ele.getTrack().isKalmanTrack()) ele_nhits*=2;
-  int pos_nhits = pos.getTrack().getTrackerHitCount();
-  if (not pos.getTrack().isKalmanTrack()) pos_nhits*=2;
-  cf_.apply("clusters_within_1.45ns", cluster_tdiff < 1.45);
-  cf_.apply("electron_below_1.75GeV", ele.getTrack().getP() < 1.75);
-  cf_.apply("vertex_chi2", vtx.getChi2() < 20.0);
-  cf_.apply("ele_track_chi2ndf", ele.getTrack().getChi2Ndf() < 20.0);
-  cf_.apply("pos_track_chi2ndf", pos.getTrack().getChi2Ndf() < 20.0);
-  cf_.apply("ele_min_8_hits", ele_nhits > 7);
-  cf_.apply("pos_min_8_hits", pos_nhits > 7);
-  // left timing until last since it is having the biggest effect besides
-  // requiring a vertex
-  double ele_track_cluster_tdiff{
-        abs(ele.getTrack().getTrackTime()-ele.getCluster().getTime())
-  };
-  double pos_track_cluster_tdiff{
-        abs(pos.getTrack().getTrackTime()-pos.getCluster().getTime())
-  };
-  cf_.apply("ele_track_before_6ns", ele.getTrack().getTrackTime() < 6.0);
-  cf_.apply("pos_track_before_6ns", pos.getTrack().getTrackTime() < 6.0);
-  cf_.apply("ele_track_cluster_within_4ns", ele_track_cluster_tdiff < 4.0);
-  cf_.apply("pos_track_cluster_within_4ns", pos_track_cluster_tdiff < 4.0);
-
-  cf_.fill_nm1("clusters_within_1.45ns", cluster_tdiff);
-  cf_.fill_nm1("electron_below_1.75GeV", ele.getTrack().getP());
-  cf_.fill_nm1("vertex_chi2", vtx.getChi2());
-  cf_.fill_nm1("ele_track_chi2ndf", ele.getTrack().getChi2Ndf());
-  cf_.fill_nm1("pos_track_chi2ndf", pos.getTrack().getChi2Ndf());
-  cf_.fill_nm1("ele_min_8_hits", ele_nhits);
-  cf_.fill_nm1("pos_min_8_hits", pos_nhits);
-  cf_.fill_nm1("ele_track_before_6ns", ele.getTrack().getTrackTime());
-  cf_.fill_nm1("pos_track_before_6ns", pos.getTrack().getTrackTime());
-  cf_.fill_nm1("ele_track_cluster_within_4ns", ele_track_cluster_tdiff);
-  cf_.fill_nm1("pos_track_cluster_within_4ns", pos_track_cluster_tdiff);
-
-  if (not cf_.keep()) {
-    return true;
-  }
-   */
-
-  /**
    * This is where the output TTree is filled,
    * if we leave before this point, then the event will not
    * be kept as part of pre-selection.
@@ -463,7 +499,8 @@ void PreSelectAndCategorize::finalize() {
   outF_->cd();
   output_tree_->Write();
   n_vertices_h_->Write();
-  cf_.save();
+  vertex_cf_.save();
+  event_cf_.save();
 }
 
 DECLARE_PROCESSOR(PreSelectAndCategorize);
