688b2167a2c354db642d14479b7c167bed0c7e34
Alic <47487490+alspellm@users.noreply.github.com>
2024-06-20 07:35:27 -0700

    Update utils/src/TrackSmearingTool.cxx
    Co-authored-by: Tom Eichlersmith <31970302+tomeichlersmith@users.noreply.github.com>

--- utils/src/TrackSmearingTool.cxx
+++ utils/src/TrackSmearingTool.cxx
@@ -76,17 +76,9 @@ double TrackSmearingTool::smearTrackP(const Track& track) {
 }
 
 double TrackSmearingTool::updateWithSmearP(Track& trk, Particle* part) {
-    double smeared_magnitude = smearTrackP(trk);
-    // updated momentum by scaling each coordinate by smeared/unsmeared
-    // this takes the direction of the unsmeared momentum and applies
-    // the smeared magnitude
-    std::vector<double> momentum = trk.getMomentum(); 
-    double unsmeared_magnitude = trk.getP();
-    for (double& coordinate : momentum)
-        coordinate *= (smeared_magnitude/unsmeared_magnitude);
-    trk.setMomentum(momentum);
-    part->setTrack(&trk);
-    return (smeared_magnitude/unsmeared_magnitude);
+    double smear_factor = updateWithSmearP(trk);
+    part->setTrack(&trk); // copy updated track into Particle
+    return smear_factor;
 }
 
 double TrackSmearingTool::updateWithSmearP(Track& trk) {
