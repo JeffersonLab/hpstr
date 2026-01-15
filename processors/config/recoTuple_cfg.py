edbac1c9dcc7a5e792d7843b83cd0c4c1c60ec45
Matthew Gignac <mgignac@slac.stanford.edu>
2026-01-15 09:53:19 -0800

    Target

--- processors/config/recoTuple_cfg.py
+++ processors/config/recoTuple_cfg.py
@@ -87,7 +87,6 @@ track.parameters["trkhitCollRoot"] = 'SiClustersOnTrack'
 track.parameters["hitFitsCollLcio"] = 'SVTFittedRawTrackerHits'
 track.parameters["rawhitCollRoot"] = 'SVTRawHitsOnTrack_KF'
 track.parameters["trackStateLocation"]='AtTarget'
-
 if(options.useBField):
     track.parameters["bfield"] = bfield[str(options.year)]
 
